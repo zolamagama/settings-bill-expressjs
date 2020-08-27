b1f5f62e0c3c57e65085cdf1c3544b896d1fd729
Zola Magama <zolamagama1985@gmail.com>
2020-08-27 15:25:44 +0200

    commit these changes

--- settings-bill.js
+++ settings-bill.js
@@ -138,21 +138,7 @@ if (!hasReachedCriticalLevel()){
         totals,
         totalClassName,
         hasReachedWarningLevel
-        // hasReachedCriticalLevel,
-        // setCallCost,
-        // getCallCost,
-        // setSmsCost,
-        // getSmsCost,
-        // setWarningLevel,
-        // getWarningLevel,
-        // setCriticalLevel,
-        // getCriticalLevel,
-        // calculateTot,
-        // getTotalCallCost,
-        // getTotalSmsCost,
-        // getTotalCost,
-        // totalClassName,
-        // hasReachedTheCriticalLevel
+        
 
     };
 }
@@ -163,90 +149,3 @@ if (!hasReachedCriticalLevel()){
 
 
 
-    // function setCallCost(callCost) {
-    //     theCallCost = callCost;
-
-    // }
-
-    // function getCallCost() {
-
-    //     return theCallCost;
-    // }
-
-    // function setSmsCost(smsCost) {
-    //     theSmsCost = smsCost;
-
-    // }
-
-    // function getSmsCost() {
-    //     return theSmsCost;
-    // }
-
-    // function setWarningLevel(warningLevel) {
-    //     theWarningLevel = warningLevel;
-    // }
-
-    // function getWarningLevel() {
-    //     return theWarningLevel;
-    // }
-
-    // function setCriticalLevel(criticalLevel) {
-    //     theCriticalLevel = criticalLevel;
-    // }
-
-    // function getCriticalLevel() {
-    //     return theCriticalLevel;
-    // }
-
-    // function calculateTot(name) {
-    //     if (!hasReachedTheCriticalLevel()) {
-    //         if (name === "call") {
-    //             callTotalCost += theCallCost;
-    //         }
-    //         if (name === "sms") {
-    //             smsTotalCost += theSmsCost;
-    //         }
-    //     }
-    // }
-
-    // function getTotalCost() {
-
-    //     return callTotalCost + smsTotalCost;
-    // }
-
-    // function getTotalCallCost() {
-    //     return callTotalCost;
-    // }
-
-    // function getTotalSmsCost() {
-    //     return smsTotalCost;
-    // }
-
-    // // // function sendSms() {
-    // // //     if (!hasReachedTheCriticalLevel()) {
-    // // //         smsTotalCost += theSmsCost;
-    // // //     }
-
-
-    // // }
-
-    // function hasReachedTheCriticalLevel() {
-    //     return getTotalCost() >= getCriticalLevel();
-
-    // }
-
-    // function totalClassName() {
-    //     if (hasReachedTheCriticalLevel()) {
-    //         return "danger";
-    //     }
-
-    //     if (getTotalCost() >= getWarningLevel()) {
-    //         return "warning";
-    //     }
-
-    // }
-
-
-
-
-// }
